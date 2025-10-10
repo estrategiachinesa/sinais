f3040fe5fcb602d99c8fddb871fadc7e512b9e1c
Trader Chines <traderchinesvip@gmail.com>
2025-10-10 04:52:43 +0000

    troque o video para esse https://youtube.com/shorts/TmK2mnpXT7A

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function GatePage() {
                 Instruções
               </h2>
               <div className="aspect-video w-full rounded-lg overflow-hidden">
-                <CustomVideoPlayer url="https://youtu.be/1iAgu5fAod0" />
+                <CustomVideoPlayer url="https://youtube.com/shorts/TmK2mnpXT7A" />
               </div>
             </div>
           </div>
